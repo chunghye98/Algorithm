33a4e222618bece2264d030e30180dee48cb6c71
hyeseon <chunghye1998@gmail.com>
2024-01-18 17:45:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.01 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+		for (String p : participant) {
+			map.put(p, map.getOrDefault(p, 0) + 1);
+		}
+
+		for (String c : completion) {
+			map.put(c, map.get(c) - 1);
+		}
+
+		String result = "";
+		for (String key : map.keySet()) {
+			if (map.get(key) > 0) {
+				result = key;
+			}
+		}
+		return result;
+    }
+}
