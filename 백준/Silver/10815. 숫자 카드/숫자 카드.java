7dcd1a1104db0a08a815c8c22739c32587cc08a9
hyeseon <chunghye1998@gmail.com>
2024-03-27 15:52:07 +0900

    [Silver V] Title: 숫자 카드, Time: 1324 ms, Memory: 123028 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -3,45 +3,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+	static int[] realCards;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] cardsIHave = new int[n];
+		realCards = new int[n];
 		for (int i = 0; i < n; i++) {
-			cardsIHave[i] = Integer.parseInt(st.nextToken());
+			realCards[i] = Integer.parseInt(st.nextToken());
 		}
+		Arrays.sort(realCards);
 
 		int m = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
-		int[] cardsToIdentify = new int[m];
+		int[] determineCards = new int[m];
 		for (int i = 0; i < m; i++) {
-			cardsToIdentify[i] = Integer.parseInt(st.nextToken());
+			determineCards[i] = Integer.parseInt(st.nextToken());
 		}
-
-		Arrays.sort(cardsIHave);
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < m; i++) {
-			sb.append(binarySearch(cardsIHave, cardsToIdentify[i], 0, n - 1) + " ");
+			sb.append(binarySearch(determineCards[i]) + " ");
 		}
 
 		System.out.println(sb);
 	}
 
-	private static int binarySearch(int[] cardsIHave, int target, int start, int end) {
+	private static int binarySearch(int determineCard) {
+		int start = 0;
+		int end = realCards.length - 1;
+
 		while (start <= end) {
 			int mid = (start + end) / 2;
 
-			if (cardsIHave[mid] == target) {
-				return 1;
-			} else if (cardsIHave[mid] > target) {
+			if (determineCard < realCards[mid]) {
 				end = mid - 1;
-			} else {
+			} else if (determineCard > realCards[mid]) {
 				start = mid + 1;
+			} else {
+				return 1;
 			}
 		}
 		return 0;
 	}
+
 }
