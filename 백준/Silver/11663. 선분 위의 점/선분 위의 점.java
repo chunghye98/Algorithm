d10297fc45a7ee0891f53262652759022adfbfb3
hyeseon <chunghye1998@gmail.com>
2024-03-29 22:58:57 +0900

    [Silver III] Title: 선분 위의 점, Time: 856 ms, Memory: 58084 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.java
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.java
@@ -2,47 +2,54 @@
 import java.io.*;
 import java.util.*;
 
+/*
+1. n, m 입력
+2. n만큼 points 입력받음, 정렬
+3. m만큼 반복해서 선분 입력
+	1. 선분의 첫번째와 끝을 입력받음
+	2. 선분의 첫번째를 포함하는 points 인덱스 찾기
+	3. 선분의 끝을 포함하는 points 인덱스 찾기 -> end + 1
+	4. 끝인덱스 - 첫인덱스 => 선분 위의 모든 점
+ */
 public class Main {
 
-	private static long[] points;
+	static int[] points;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int pointN = Integer.parseInt(st.nextToken());
-		int edgeN = Integer.parseInt(st.nextToken());
+		int lineN = Integer.parseInt(st.nextToken());
 
-		points = new long[pointN];
-		st = new StringTokenizer(br.readLine());
+		points = new int[pointN];
+
+		st= new StringTokenizer(br.readLine());
 		for (int i = 0; i < pointN; i++) {
-			points[i] = Long.parseLong(st.nextToken());
+			points[i] = Integer.parseInt(st.nextToken());
 		}
 
 		Arrays.sort(points);
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < edgeN; i++) {
+		for (int i = 0; i < lineN; i++) {
 			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
 
-			long first = Long.parseLong(st.nextToken());
-			long second = Long.parseLong(st.nextToken());
-
-			sb.append(binarySearch(first, second));
+			sb.append(binarySearch(s, e));
 			sb.append("\n");
 		}
 		System.out.println(sb);
-
 	}
 
-	private static int binarySearch(long first, long second) {
+	private static int binarySearch(int s, int e) {
 		int start = 0;
 		int end = points.length - 1;
 
 		while (start <= end) {
 			int mid = (start + end) / 2;
 
-			if (first <= points[mid]) {
+			if (s <= points[mid]) {
 				end = mid - 1;
 			} else {
 				start = mid + 1;
@@ -57,15 +64,16 @@ public class Main {
 		while (start <= end) {
 			int mid = (start + end) / 2;
 
-			if (second < points[mid]) {
-				end = mid - 1;
-			} else {
+			if (e >= points[mid]) {
 				start = mid + 1;
+			} else {
+				end = mid - 1;
 			}
 		}
 
-		int endIndex = end + 1;
+		int endIndex = end + 1; // 끝점도 포함하기 위함
 
 		return endIndex - startIndex;
 	}
 }
+
