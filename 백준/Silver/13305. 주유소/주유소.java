fd7c46e0121fecd1873f4f49d5edb699c74415a8
hyeseon <chunghye1998@gmail.com>
2024-04-02 23:28:01 +0900

    Update 주유소.java

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -5,35 +5,26 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		int cityN = Integer.parseInt(br.readLine());
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		long[] distance = new long[n - 1];
-		for (int i = 0; i < n - 1; i++) {
-			distance[i] = Integer.parseInt(st.nextToken());
+		long[] paths = new long[cityN - 1];
+		for (int i = 0; i < cityN - 1; i++) {
+			paths[i] = Long.parseLong(st.nextToken());
 		}
 
 		st = new StringTokenizer(br.readLine());
-		long[] price = new long[n];
-		for (int i = 0; i < n; i++) {
-			price[i] = Long.parseLong(st.nextToken());
+		long[] costs = new long[cityN];
+		for (int i = 0; i < cityN; i++) {
+			costs[i] = Long.parseLong(st.nextToken());
 		}
 
-		long minPrice = solve(distance, price);
-
-		System.out.println(minPrice);
-	}
-
-	private static long solve(long[] distance, long[] price) {
-		long sum = 0;
-		long min = price[0];
-
-		for(int i=0; i<distance.length; i++) {
-			if(price[i] < min) {
-				min = price[i];
-			}
-			sum += min * distance[i];
+		long min = costs[0];
+		long sum = costs[0] * paths[0];
+		for (int i = 1; i < cityN - 1; i++) {
+			min = Math.min(min, costs[i]);
+			sum += min * paths[i];
 		}
-		return sum;
+		System.out.println(sum);
 	}
 }
