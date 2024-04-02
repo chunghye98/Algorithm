d5ec8c16de8a4950b4e111b1478b8ef746fb9a1e
hyeseon <chunghye1998@gmail.com>
2024-04-02 22:57:20 +0900

    [Gold IV] Title: 공유기 설치, Time: 304 ms, Memory: 29348 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -8,11 +8,10 @@ import java.util.*;
 public class Main {
 
 	static int[] house;
-	static int n;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 
 		house = new int[n];
@@ -22,53 +21,43 @@ public class Main {
 
 		Arrays.sort(house);
 
-		System.out.println(binarySearch(c) - 1);
+		System.out.println(binarySearch(c));
 	}
 
 	private static int binarySearch(int c) {
-		int start = 1; // 최소 거리가 가질 수 있는 최솟값
-		int end = house[n - 1] - house[0] + 1; // 최소 거리가 가질 수 있는 최댓값
+		// 공유기 간의 최대 거리를 찾는데 사용
+		int start = 1;
+		int end = house[house.length - 1] - house[0];
 
-		while (start < end) {
+		while (start <= end) {
 			int mid = (start + end) / 2;
 
-			/*
-			 * mid 거리에 대해 설치 가능한 공유기 개수가 M 개수에 못미치면
-			 * 거리를 좁혀야 하기 때문에 end 를 줄인다.
-			 */
+			// mid까지 설치된 공유기의 대수가 원하는 값보다 작으면 거리를 늘린다.
 			if (isInstall(mid) < c) {
-				end = mid;
-			} else {
-				/*
-				 * 설치 가능한 공유기 개수가 c 개수보다 크거나 같으면
-				 * 거리를 벌리면서 최소거리가 가질 수 있는 최대 거리를
-				 * 찾아낸다.
-				 */
+				end = mid - 1;
+			}
+			// mid까지 설치된 공유기의 대수가 원하는 값보다 크면 같은 경우 거리를 늘린다.
+			else {
 				start = mid + 1;
 			}
 		}
-		return start;
+		return start - 1; // 공유기 간의 최대 간격, start: 공유기를 설치할 수 없는 거리의 최솟값
 	}
 
-	// distance에 대해 설치 가능한 공유기 개수를 찾는 메소드
-	private static int isInstall(int distance) {
-		// 첫 번째 집은 무조건 설치
+	private static int isInstall(int mid) {
+		// 첫번째 집은 무조건 설치
 		int count = 1;
-		int lastLocate = house[0];
+		int lastLocated = house[0];
 
 		for (int i = 1; i < house.length; i++) {
 			int locate = house[i];
 
-			/*
-			 * 현재 탐색하는 집의 위치와 직전에 설치했던 집의 위치간 거리가
-			 * 최소 거리(distance)보다 크거나 같을 때 공유기 설치 개수를 늘려주고
-			 * 마지막 설치 위치를 갱신
-			 */
-			if (locate - lastLocate >= distance) {
+			if (locate - lastLocated >= mid) {
 				count++;
-				lastLocate = locate;
+				lastLocated = locate;
 			}
 		}
 		return count;
 	}
+
 }
