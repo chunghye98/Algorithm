319f17d304693dbee2f8e3807f69c666b06b890b
hyeseon <chunghye1998@gmail.com>
2024-07-18 15:36:04 +0900

    Update 수학숙제.java

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -3,6 +3,13 @@ import java.math.*;
 import java.util.*;
 import java.util.regex.*;
 
+/*
+1. n을 입력받는다.
+2. 문자열 입력받은 후 해당 문자열에서 0-9 사이 숫자 뭉텅이를 찾는다.
+3. 찾은 숫자들을 ArrayList에 넣는다. 이때 타입은 BigInteger로 넣어야 NumberformatException이 발생하지 않는다.
+4. ArrayList를 오름차순 정렬한다.
+5. ArrayList의 요소를 출력한다. 
+*/
 public class Main {
 
 	public static void main(String[] args) throws Exception{
@@ -18,7 +25,7 @@ public class Main {
 			
 			while(numberMatcher.find()) {
 				String matched = numberMatcher.group();
-	            numbers.add(new BigInteger(matched));
+	            		numbers.add(new BigInteger(matched));
 			}
 		}
 		Collections.sort(numbers);
