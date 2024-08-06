13a6d3ebbc8b357802f4afde07974f783a766744
hyeseon <chunghye1998@gmail.com>
2024-08-06 16:36:58 +0900

    [Silver II] Title: 색종이 만들기, Time: 144 ms, Memory: 16096 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int[][] map;
+    static int n;
+    static Map<Integer, Integer> count = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve(0, 0, n, n);
+        output();
+    }
+
+    private static void output() {
+        for (int key : count.keySet()) {
+            sb.append(count.get(key)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void solve(int sy, int sx, int ey, int ex) {
+        if(ey - sy == 1 || ex - sx == 1) {
+                count.put(map[sy][sx], count.get(map[sy][sx]) + 1);
+            return;
+        }
+
+        boolean isEqual = isEqual(sy, sx, ey, ex);
+        if(isEqual) {
+            count.put(map[sy][sx], count.get(map[sy][sx]) + 1);
+            return;
+        }
+
+        // 왼상
+        solve(sy, sx, (sy + ey) / 2, (sx + ex) / 2);
+        // 오상
+        solve(sy, (sx + ex) / 2, (sy + ey) / 2, ex);
+        // 오하
+        solve((sy + ey) / 2, (sx + ex) / 2, ey, ex);
+        // 왼하
+        solve((sy + ey) / 2, sx, ey, (sx + ex) / 2);
+
+
+    }
+
+    private static boolean isEqual(int sy, int sx, int ey, int ex) {
+        int before = map[sy][sx];
+        for (int i = sy; i < ey; i++) {
+            for (int j = sx; j < ex; j++) {
+                if(before != map[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void init() throws IOException {
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] inputs = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(inputs[j]);
+            }
+        }
+
+        count.put(0, 0);
+        count.put(1, 0);
+    }
+}
