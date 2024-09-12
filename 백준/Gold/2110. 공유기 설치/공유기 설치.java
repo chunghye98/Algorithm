1881863167eafa8bc0485bdefc2d180bef92155f
hyeseon <chunghye1998@gmail.com>
2024-09-12 10:45:43 +0900

    [Gold IV] Title: 공유기 설치, Time: 260 ms, Memory: 28936 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,63 +1,67 @@
-
 import java.io.*;
 import java.util.*;
 
-/*
- * 답안 참고: https://st-lab.tistory.com/277
- */
 public class Main {
 
-	static int[] house;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int C;
+    static int[] homes;
 
-		house = new int[n];
-		for (int i = 0; i < n; i++) {
-			house[i] = Integer.parseInt(br.readLine());
-		}
+    static int result;
 
-		Arrays.sort(house);
+    public static void main(String[] args) throws Exception {
+        init();
+        solve();
+        output();
+    }
 
-		System.out.println(binarySearch(c));
-	}
+    public static void output() {
+        System.out.println(result);
+    }
 
-	private static int binarySearch(int c) {
-		// 공유기 간의 최대 거리를 찾는데 사용
-		int start = 1;
-		int end = house[house.length - 1] - house[0];
+    public static void solve() {
+        int start = 1; // 최소 범위
+        int end = homes[homes.length - 1] - homes[0]; // 최대 범위
 
-		while (start <= end) {
-			int mid = (start + end) / 2;
+        while(start <= end) { // <= 을 사용하는 이유: start와 end가 같을 때까지, 그 값도 확인하기 위함이다.
+            int mid = (start + end) / 2;
 
-			// mid까지 설치된 공유기의 대수가 원하는 값보다 작으면 거리를 늘린다.
-			if (isInstall(mid) < c) {
-				end = mid - 1;
-			}
-			// mid까지 설치된 공유기의 대수가 원하는 값보다 크면 같은 경우 거리를 늘린다.
-			else {
-				start = mid + 1;
-			}
-		}
-		return start - 1; // 공유기 간의 최대 간격, start: 공유기를 설치할 수 없는 거리의 최솟값
-	}
+            // mid로 공유기 설치가 가능하거나 더 넓은 범위로도 설치가 가능한 경우
+            if(getCount(mid) >= C) {
+                result = mid; // 두 공유기 사이의 거리가 최대여야 하므로 계속 갱신
+                start = mid + 1; // 더 넓은 범위에서 설치가 가능한지 확인
+            }else {
+                end = mid - 1; // 더 작은 범위에서 설치가 가능한지 확인
+            }
+        }
+    }
 
-	private static int isInstall(int mid) {
-		// 첫번째 집은 무조건 설치
-		int count = 1;
-		int lastLocated = house[0];
+    public static int getCount(int mid) {
+        int count = 1; // 첫번째 집에 무조건 공유기 설치
+        int before = homes[0];
 
-		for (int i = 1; i < house.length; i++) {
-			int locate = house[i];
+        for (int i = 1; i < N; i++) {
+            // 인덱스가 3일 때 집의 좌표는 8이다. 8-1 >= 3 이므로 공유기를 하나 더 설치해야 한다.
+            if(homes[i] - before >= mid) {
+                count++;
+                before = homes[i];
+            }
+        }
+        return count;
+    }
 
-			if (locate - lastLocated >= mid) {
-				count++;
-				lastLocated = locate;
-			}
-		}
-		return count;
-	}
+    public static void init() throws Exception {
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        C = Integer.parseInt(inputs[1]);
 
+        homes = new int[N];
+        for (int i = 0; i < N; i++) {
+            homes[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(homes);
+    }
 }
