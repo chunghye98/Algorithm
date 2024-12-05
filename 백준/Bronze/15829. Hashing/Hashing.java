56daf964c6d91927ef2bed729563b8acd696387c
hyeseon <chunghye1998@gmail.com>
2024-12-05 17:43:00 +0900

    [Bronze II] Title: Hashing, Time: 116 ms, Memory: 14184 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,16 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        char[] word = br.readLine().toCharArray();
+	static final int MOD = 1234567891;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static int[] str;
+	static long result;
 
-        int result = 0;
-        for(int i = 0; i < word.length; i++){
-            result += (word[i] - ('a' - 1)) * Math.pow(31, i);
-        }
-        System.out.println(result);
-    }
+	public static void main(String[] args) throws Exception {
+		init();
+		solve();
+		output();
+	}
+
+	public static void init() throws Exception {
+		N = Integer.parseInt(br.readLine());
+		str = new int[N];
+		char[] inputs = br.readLine().toCharArray();
+		for (int i = 0; i < N; i++) {
+			str[i] = inputs[i] - 'a' + 1;
+		}
+	}
+
+	private static void solve() {
+		result = 0;
+		long power = 1;
+		for (int value : str) {
+			result = (result + value * power) % MOD;
+			power = (power * 31) % MOD;
+		}
+	}
+
+	private static void output() {
+		System.out.println(result);
+	}
 }
