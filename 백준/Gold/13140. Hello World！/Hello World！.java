61d597ed862fe58375cdbfdff1dec51c1ab7f24f
hyeseon <chunghye1998@gmail.com>
2024-12-26 01:47:53 +0900

    [Gold V] Title: Hello World!, Time: 228 ms, Memory: 72936 KB -BaekjoonHub

--- 백준/Gold/13140. Hello World！/Hello World！.java
+++ 백준/Gold/13140. Hello World！/Hello World！.java
@@ -23,13 +23,9 @@ public class Main {
 			String strFirst = String.valueOf(i);
 			String strSecond = String.valueOf(N - i);
 
-			// 문자열 길이 체크
-			if (strFirst.length() != 5 || strSecond.length() != 5) continue;
+			if (strFirst.length() != 5 || strSecond.length() != 5)
+				continue;
 
-			// h와 w가 0인지 체크
-			if (strFirst.charAt(0) == '0' || strSecond.charAt(0) == '0') continue;
-
-			// 특정 자리 조건 체크
 			if (!(strFirst.charAt(2) == strFirst.charAt(3) && strFirst.charAt(3) == strSecond.charAt(3))) {
 				continue;
 			}
@@ -38,18 +34,17 @@ public class Main {
 				continue;
 			}
 
-			// 중복 숫자 체크
 			Set<Character> usedDigits = new HashSet<>();
 			boolean isFail = false;
 			for (int j = 0; j < 5; j++) {
 				if (j == 3) {
-					continue; // 3번째 자리는 조건 제외
-				}else if (j == 4) {
+					continue;
+				} else if (j == 4) {
 					if (!usedDigits.add(strSecond.charAt(j))) {
 						isFail = true;
 						break;
 					}
-				}else if (!usedDigits.add(strFirst.charAt(j)) || !usedDigits.add(strSecond.charAt(j))) {
+				} else if (!usedDigits.add(strFirst.charAt(j)) || !usedDigits.add(strSecond.charAt(j))) {
 					isFail = true;
 					break;
 				}
@@ -72,7 +67,7 @@ public class Main {
 			sb.append("-------\n");
 			if (String.valueOf(N).length() > 5) {
 				sb.append(" ").append(N);
-			}else {
+			} else {
 				sb.append("  ").append(N);
 			}
 		}
